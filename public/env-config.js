376f7088ee2f9caebfd7f28fc5869c509e14e373
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2026-01-12 23:19:43 -0300

    chore: enable API_URL configuration in env-config.js

--- public/env-config.js
+++ public/env-config.js
@@ -1,5 +1,5 @@
 window.ENV = {
-    // API_URL: 'http://localhost:3000/api',
+    API_URL: 'http://localhost:3000/api',
     APP_DEBUG: false,
     GITHUB_APP_NAME: 'driftive',
     // POSTHOG_API_KEY: 'phc_xxx',
